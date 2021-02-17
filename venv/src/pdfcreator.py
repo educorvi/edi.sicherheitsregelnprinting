f27a87866ad97cbb8ea55e7686f68a968970ad8d
Seppo Walther <seppo.walther@educorvi.de>
2021-02-17 10:57:52 +0100

    Adding fields

--- venv/src/pdfcreator.py
+++ venv/src/pdfcreator.py
@@ -7,11 +7,19 @@ pdf.add_page()
 pdf.image("testvorlage-JPG.jpg", x=-4, y=-8, w=217, h=313)
 
 pdf.set_font('Arial', '', 14)
-
 pdf.set_xy(108, 31)
 pdf.cell(0, 0, 'Wie erfolgte die Freischaltung?')
 
+pdf.set_font('Arial', 'u', 14)
 pdf.set_xy(108, 36)
-pdf.cell(0, 0, 'Wie erfolgte die Freischaltung?')
+pdf.cell(0, 0, 'NH-Sicherungen')
+
+pdf.set_font('Arial', '', 14)
+pdf.set_xy(108, 46)
+pdf.cell(0, 0, 'Auslösestrom NH-Sicherungen:')
+
+pdf.set_font('Arial', '', 14)
+pdf.set_xy(108, 51)
+pdf.cell(0, 0, '_________ A')
 
 pdf.output("sicherheitsregeln.pdf", "F")
