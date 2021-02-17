13c44e4ab1d58748becba1a46473871cd15bda1f
Seppo Walther <seppo.walther@educorvi.de>
2021-02-17 11:46:30 +0100

    Adding fields

--- venv/src/pdfcreator.py
+++ venv/src/pdfcreator.py
@@ -4,7 +4,7 @@ pdf = FPDF(orientation='P', unit='mm', format='A4')
 pdf.add_page()
 
 #pdf.image("5-Sicherheitsregeln-Vorlage-JPG.jpg", x=-4, y=-8, w=217, h=313)
-pdf.image("testvorlage-JPG.jpg", x=-4, y=-8, w=217, h=313)
+pdf.image("vorlage-JPG.jpg", x=-4, y=-8, w=217, h=313)
 
 pdf.set_font('Arial', '', 14)
 pdf.set_xy(108, 31)
