8ab815d0152ddf4ef4a7c0ea5f4d7d3b4aa3e82a
Seppo Walther <seppo.walther@educorvi.de>
2021-02-18 11:34:53 +0100

    Adding fields

--- venv/src/elektrische-betriebsmittel.py
+++ venv/src/elektrische-betriebsmittel.py
@@ -41,11 +41,52 @@ pdf.set_font('Arial', 'u', 14)
 pdf.set_xy(108, 93)
 pdf.cell(0, 0, 'ja')
 
+pdf.set_font('Arial', '', 14)
+pdf.set_xy(108, 103)
+pdf.cell(0, 0, 'Wurde eine Schaltsperre eingesetzt, weil')
+
+pdf.set_font('Arial', '', 14)
+pdf.set_xy(108, 108)
+pdf.cell(0, 0, 'der Bereich fuer Laien zugaenglich ist?')
+
+pdf.set_font('Arial', 'u', 14)
+pdf.set_xy(108, 113)
+pdf.cell(0, 0, 'ja')
+
+pdf.set_font('Arial', '', 14)
+pdf.set_xy(108, 123)
+pdf.cell(0, 0, 'Wurde ein Reparaturschalter mit einem')
+
+pdf.set_font('Arial', '', 14)
+pdf.set_xy(108, 128)
+pdf.cell(0, 0, 'Vorhaengeschloss versehen?')
+
+pdf.set_font('Arial', 'u', 14)
+pdf.set_xy(108, 133)
+pdf.cell(0, 0, 'ja')
+
+pdf.set_font('Arial', '', 14)
+pdf.set_xy(108, 143)
+pdf.cell(0, 0, 'Wurde ein Schild "Schalten verboten"')
+
+pdf.set_font('Arial', '', 14)
+pdf.set_xy(108, 148)
+pdf.cell(0, 0, 'zusaetzlich angebracht?')
+
+pdf.set_font('Arial', 'u', 14)
+pdf.set_xy(108, 153)
+pdf.cell(0, 0, 'magnetisch')
+
+
 # 3 Spannungsfreiheit allpolig festgestellt an der Arbeitsstelle
 
 pdf.set_font('Arial', '', 14)
 pdf.set_xy(108, 168)
-pdf.cell(0, 0, '')
+pdf.cell(0, 0, 'Zweipoliger Spannungsprüfer:')
+
+pdf.set_font('Arial', 'u', 14)
+pdf.set_xy(108, 173)
+pdf.cell(0, 0, '3M Voltage Meter x559m')
 
 # 4 Geerdet und kurzgeschlossen
 
