40542354667798650247fb6b0b1eb7ea5a00ceb5
Seppo Walther <seppo.walther@educorvi.de>
2021-03-02 11:01:39 +0100

    Fitting in data into new template

--- venv/src/elektrohandwerk-schaltanlagen.py
+++ venv/src/elektrohandwerk-schaltanlagen.py
@@ -1,5 +1,5 @@
 from fpdf import FPDF
-from .importdata import input
+from importdata import input
 
 pdf = FPDF(orientation='P', unit='mm', format='A4')
 pdf.add_page()
@@ -77,27 +77,27 @@ pdf.cell(0, 0, data.get("abgrenzung_arbeitsbereich_nein"))
 # 1 Freigeschaltet
 
 pdf.set_font('Arial', '', 14)
-pdf.set_xy(108, 230)
+pdf.set_xy(104, 230)
 pdf.cell(0, 0, 'Wie erfolgte die Freischaltung?')
 
 pdf.set_font('Arial', 'u', 14)
-pdf.set_xy(108, 235)
+pdf.set_xy(104, 235)
 pdf.cell(0, 0, data.get("art_der_freischaltung"))
 
 pdf.set_font('Arial', '', 14)
-pdf.set_xy(108, 245)
+pdf.set_xy(104, 245)
 pdf.cell(0, 0, 'Auslösestrom: %s A' % data.get("ausloesestrom"))
 
 pdf.set_font('Arial', '', 14)
-pdf.set_xy(108, 255)
+pdf.set_xy(104, 255)
 pdf.cell(0, 0, 'Wo erfolgte die Freischaltung?')
 
 pdf.set_font('Arial', 'u', 14)
-pdf.set_xy(108, 260)
+pdf.set_xy(104, 260)
 pdf.cell(0, 0, data.get("ort_der_freischaltung"))
 
 pdf.set_font('Arial', '', 14)
-pdf.set_xy(108, 270)
+pdf.set_xy(104, 270)
 pdf.cell(0, 0, 'Nr. oder Bezeichnung: %s' % data.get("ort_nroderbezeichnung"))
 
 #Adding new page
@@ -108,89 +108,89 @@ pdf.image("vorlage5-Seite2.jpg", x=-4, y=-8, w=217, h=313)
 # 2 Gegen Wiedereinschalten gesichert
 
 pdf.set_font('Arial', '', 14)
-pdf.set_xy(108, 22)
+pdf.set_xy(104, 22)
 pdf.cell(0, 0, 'Wurde ein Vorhängeschloss am')
 
 pdf.set_font('Arial', '', 14)
-pdf.set_xy(108, 27)
+pdf.set_xy(104, 27)
 pdf.cell(0, 0, 'Schalter eingehängt und abgeschlossen?')
 
 pdf.set_font('Arial', 'u', 14)
-pdf.set_xy(108, 33)
+pdf.set_xy(104, 33)
 pdf.cell(0, 0, data.get("vorhaengeschloss_schalter"))
 
 pdf.set_font('Arial', '', 14)
-pdf.set_xy(108, 43)
+pdf.set_xy(104, 43)
 pdf.cell(0, 0, 'Wurde die Tür zum elektrischen')
 
 pdf.set_font('Arial', '', 14)
-pdf.set_xy(108, 48)
+pdf.set_xy(104, 48)
 pdf.cell(0, 0, 'Betriebsraum verschlossen?')
 
 pdf.set_font('Arial', 'u', 14)
-pdf.set_xy(108, 53)
+pdf.set_xy(104, 53)
 pdf.cell(0, 0, data.get("betriebsraum_tuer_verschlossen"))
 
 pdf.set_font('Arial', '', 14)
-pdf.set_xy(108, 63)
+pdf.set_xy(104, 63)
 pdf.cell(0, 0, 'Wurde ein Schild "Schalten verboten"')
 
 pdf.set_font('Arial', '', 14)
-pdf.set_xy(108, 68)
+pdf.set_xy(104, 68)
 pdf.cell(0, 0, 'zusätzlich angebracht?')
 
 pdf.set_font('Arial', 'u', 14)
-pdf.set_xy(108, 73)
+pdf.set_xy(104, 73)
 pdf.cell(0, 0, data.get("schalten_verboten"))
 
 # 3 Spannungsfreiheit allpolig festgestellt an der Arbeitsstelle
 
 pdf.set_font('Arial', '', 14)
-pdf.set_xy(108, 89)
+pdf.set_xy(104, 89)
 pdf.cell(0, 0, 'Zweipoliger Spannungsprüfer:')
 
 pdf.set_font('Arial', 'u', 14)
-pdf.set_xy(108, 99)
+pdf.set_xy(104, 99)
 pdf.cell(0, 0, data.get("spannungspruefer"))
 
 # 4 Geerdet und kurzgeschlossen
 
 pdf.set_font('Arial', '', 14)
-pdf.set_xy(108, 130)
+pdf.set_xy(104, 130)
 pdf.cell(0, 0, 'Wo wurde die EuK-Vorrichtung')
 
 pdf.set_font('Arial', '', 14)
-pdf.set_xy(108, 135)
+pdf.set_xy(104, 135)
 pdf.cell(0, 0, 'eingebaut?')
 
 pdf.set_font('Arial', 'u', 14)
-pdf.set_xy(108, 140)
+pdf.set_xy(104, 140)
 pdf.cell(0, 0, data.get("euk_wo_eingebaut"))
 
 pdf.set_font('Arial', '', 14)
-pdf.set_xy(108, 150)
+pdf.set_xy(104, 150)
 pdf.cell(0, 0, 'Begründung:')
 
 pdf.set_font('Arial', 'u', 14)
-pdf.set_xy(108, 155)
+pdf.set_xy(104, 155)
 pdf.cell(0, 0, data.get("geerdet_begruendung"))
 
 # 5 Mit der Abdeckung soll erreicht werden
 
 pdf.set_font('Arial', '', 14)
-pdf.set_xy(108, 170)
+pdf.set_xy(104, 170)
 pdf.cell(0, 0, 'Mit der Abdeckung soll erreicht werden:')
 
 pdf.set_font('Arial', 'u', 14)
-pdf.set_xy(108, 175)
+pdf.set_xy(104, 175)
 pdf.cell(0, 0, data.get("ziel_der_abdeckung"))
 
 pdf.set_font('Arial', '', 14)
-pdf.set_xy(108, 185)
+pdf.set_xy(104, 185)
 pdf.cell(0, 0, 'Art der Abdeckung:')
 
 pdf.set_font('Arial', 'u', 14)
-pdf.set_xy(108, 190)
+pdf.set_xy(104, 190)
 pdf.cell(0, 0, data.get("art_der_abdeckung"))
 
 
