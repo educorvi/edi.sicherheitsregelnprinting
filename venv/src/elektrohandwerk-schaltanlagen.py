8a4082e207bfd91228951fc8bea44f276ec4b25b
Seppo Walther <seppo.walther@educorvi.de>
2021-03-02 11:23:38 +0100

    Implemented DGUVMeta in #2

--- venv/src/elektrohandwerk-schaltanlagen.py
+++ venv/src/elektrohandwerk-schaltanlagen.py
@@ -111,39 +111,39 @@ pdf.image("vorlage5-Seite2.jpg", x=-4, y=-8, w=217, h=313)
 
 # 2 Gegen Wiedereinschalten gesichert
 
-pdf.set_font('Arial', '', 14)
+pdf.set_font('DGUVMeta-Normal', '', 14)
 pdf.set_xy(104, 22)
 pdf.cell(0, 0, 'Wurde ein Vorhängeschloss am')
 
-pdf.set_font('Arial', '', 14)
+pdf.set_font('DGUVMeta-Normal', '', 14)
 pdf.set_xy(104, 27)
 pdf.cell(0, 0, 'Schalter eingehängt und abgeschlossen?')
 
-pdf.set_font('Arial', 'u', 14)
+pdf.set_font('DGUVMeta-Normal', 'u', 14)
 pdf.set_xy(104, 33)
 pdf.cell(0, 0, data.get("vorhaengeschloss_schalter"))
 
-pdf.set_font('Arial', '', 14)
+pdf.set_font('DGUVMeta-Normal', '', 14)
 pdf.set_xy(104, 43)
 pdf.cell(0, 0, 'Wurde die Tür zum elektrischen')
 
-pdf.set_font('Arial', '', 14)
+pdf.set_font('DGUVMeta-Normal', '', 14)
 pdf.set_xy(104, 48)
 pdf.cell(0, 0, 'Betriebsraum verschlossen?')
 
-pdf.set_font('Arial', 'u', 14)
+pdf.set_font('DGUVMeta-Normal', 'u', 14)
 pdf.set_xy(104, 53)
 pdf.cell(0, 0, data.get("betriebsraum_tuer_verschlossen"))
 
-pdf.set_font('Arial', '', 14)
+pdf.set_font('DGUVMeta-Normal', '', 14)
 pdf.set_xy(104, 63)
 pdf.cell(0, 0, 'Wurde ein Schild "Schalten verboten"')
 
-pdf.set_font('Arial', '', 14)
+pdf.set_font('DGUVMeta-Normal', '', 14)
 pdf.set_xy(104, 68)
 pdf.cell(0, 0, 'zusätzlich angebracht?')
 
-pdf.set_font('Arial', 'u', 14)
+pdf.set_font('DGUVMeta-Normal', 'u', 14)
 pdf.set_xy(104, 73)
 pdf.cell(0, 0, data.get("schalten_verboten"))
 
