6c9677dbd6312bd21c350b9251872a840b23ae80
Seppo Walther <seppo.walther@educorvi.de>
2021-03-03 13:45:47 +0100

    Adding datadict & Kopffragen

--- venv/src/elektrische-betriebsmittel.py
+++ venv/src/elektrische-betriebsmittel.py
@@ -9,6 +9,57 @@ pdf.add_font('DGUVMeta-NormalItalic', '', 'DGUVMeta-NormalItalic.ttf', uni=True)
 
 pdf.image("vorlage9-Seite1.jpg", x=-4, y=-8, w=217, h=313)
 
+data = {}
+
+# Kopffragen
+"""
+data["arbeitsstelle"] = input.get('#/properties/arbeitsstelle-arbeitsort')
+data["datum_uhrzeit"] = input.get('#/properties/datum-und-uhrzeit')
+data["person_anlageverantwortlichkeit"] = input.get('#/properties/person-in-der-rolle-des-anlagenverantwortlichen)
+data["person_arbeitsverantwortlichkeit"] = input.get('#/properties/person-in-der-rolle-des-arbeitsverantwortlichen')
+data["person_arbeitsausfuehrung"] = input.get('#/properties/arbeitsausfuhrende-person')
+# Multiple-Choice-Felder (?):
+# data["zusaetzliche_schutzausrüstung"] = input.get('#/properties/zusatzliche-personliche-schutzausrustung')
+"""
+
+data["arbeitsstelle"] = "educorvi GmbH & Co. KG"
+data["datum_uhrzeit"] = "01.03.2021 11:57 Uhr"
+data["person_anlageverantwortlichkeit"] = "Lars Walther"
+data["person_arbeitsverantwortlichkeit"] = "Lars Walther"
+data["person_arbeitsausfuehrung"] = "Seppo Walther"
+data["zusaetzliche_schutzausrüstung_elektrischerschlag"] = "x"
+data["zusaetzliche_schutzausrüstung_stoerlichtbogen"] = "x"
+
+# Kopffragen
+
+pdf.set_font('DGUVMeta-Normal', '', 14)
+pdf.set_xy(12, 76)
+pdf.cell(0, 0, data.get("arbeitsstelle"))
+
+pdf.set_font('DGUVMeta-Normal', '', 14)
+pdf.set_xy(12, 94)
+pdf.cell(0, 0, data.get("datum_uhrzeit"))
+
+pdf.set_font('DGUVMeta-Normal', '', 14)
+pdf.set_xy(12, 112.5)
+pdf.cell(0, 0, data.get("person_anlageverantwortlichkeit"))
+
+pdf.set_font('DGUVMeta-Normal', '', 14)
+pdf.set_xy(12, 131)
+pdf.cell(0, 0, data.get("person_arbeitsverantwortlichkeit"))
+
+pdf.set_font('DGUVMeta-Normal', '', 14)
+pdf.set_xy(12, 149.5)
+pdf.cell(0, 0, data.get("person_arbeitsausfuehrung"))
+
+pdf.set_font('DGUVMeta-Normal', '', 14)
+pdf.set_xy(14.1, 175.6)
+pdf.cell(0, 0, data.get("zusaetzliche_schutzausrüstung_elektrischerschlag"))
+
+pdf.set_font('DGUVMeta-Normal', '', 14)
+pdf.set_xy(88.4, 175.6)
+pdf.cell(0, 0, data.get("zusaetzliche_schutzausrüstung_stoerlichtbogen"))
+
 # 1 Freigeschaltet
 
 
