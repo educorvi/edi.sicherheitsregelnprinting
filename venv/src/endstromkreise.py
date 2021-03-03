4845f4415a8fb48474082dced8692bb58d0203f4
Seppo Walther <seppo.walther@educorvi.de>
2021-03-03 11:42:28 +0100

    Adding DGUVMeta to endstromkreise.py

--- venv/src/endstromkreise.py
+++ venv/src/endstromkreise.py
@@ -3,6 +3,10 @@ from fpdf import FPDF
 pdf = FPDF(orientation='P', unit='mm', format='A4')
 pdf.add_page()
 
+pdf.add_font('DGUVMeta-Normal', '', 'DGUVMeta-Normal.ttf', uni=True)
+pdf.add_font('DGUVMeta-Bold', '', 'DGUVMeta-Bold.ttf', uni=True)
+pdf.add_font('DGUVMeta-NormalItalic', '', 'DGUVMeta-NormalItalic.ttf', uni=True)
+
 pdf.image("vorlage7-Seite1.jpg", x=-4, y=-8, w=217, h=313)
 
 data = {}
