b7cbb9f95b4317ffc5654410d7b95d2f7222ae9c
Seppo Walther <seppo.walther@educorvi.de>
2021-03-08 10:24:20 +0100

    Putting in newtemplate

--- venv/src/elektrohandwerk-schaltanlagen.py
+++ venv/src/elektrohandwerk-schaltanlagen.py
@@ -8,7 +8,7 @@ pdf.add_font('DGUVMeta-Normal', '', 'DGUVMeta-Normal.ttf', uni=True)
 pdf.add_font('DGUVMeta-Bold', '', 'DGUVMeta-Bold.ttf', uni=True)
 pdf.add_font('DGUVMeta-NormalItalic', '', 'DGUVMeta-NormalItalic.ttf', uni=True)
 
-pdf.image("vorlage5-Seite1.jpg", x=-4, y=-8, w=217, h=313)
+pdf.image("newtemplate1_seite1_1.jpg", x=-4, y=-8, w=217, h=313)
 
 data = {}
 
@@ -152,7 +152,7 @@ pdf.cell(0, 0, 'Nr. oder Bezeichnung: %s' % data.get("ort_nroderbezeichnung"))
 #Adding new page
 
 pdf.add_page()
-pdf.image("vorlage5-Seite2.jpg", x=-4, y=-8, w=217, h=313)
+pdf.image("newtemplate_seite2_1.jpg", x=-4, y=-8, w=217, h=313)
 
 # 2 Gegen Wiedereinschalten gesichert
 
