580b2e204ecfa58f0c8afe62c4e6540db066afe7
Seppo Walther <seppo.walther@educorvi.de>
2021-03-08 10:32:34 +0100

    Fitting in fields into first page of new template

--- venv/src/elektrohandwerk-schaltanlagen.py
+++ venv/src/elektrohandwerk-schaltanlagen.py
@@ -88,39 +88,39 @@ data["art_der_abdeckung"] = "isolierende Formteile"
 # Kopffragen
 
 pdf.set_font('DGUVMeta-Normal', '', 14)
-pdf.set_xy(12, 101)
+pdf.set_xy(13, 101)
 pdf.cell(0, 0, data.get("arbeitsstelle"))
 
 pdf.set_font('DGUVMeta-Normal', '', 14)
-pdf.set_xy(12, 94)
+pdf.set_xy(13, 120)
 pdf.cell(0, 0, data.get("datum_uhrzeit"))
 
 pdf.set_font('DGUVMeta-Normal', '', 14)
-pdf.set_xy(12, 112.5)
+pdf.set_xy(13, 139)
 pdf.cell(0, 0, data.get("person_anlageverantwortlichkeit"))
 
 pdf.set_font('DGUVMeta-Normal', '', 14)
-pdf.set_xy(12, 131)
+pdf.set_xy(13, 158)
 pdf.cell(0, 0, data.get("person_arbeitsverantwortlichkeit"))
 
 pdf.set_font('DGUVMeta-Normal', '', 14)
-pdf.set_xy(12, 149.5)
+pdf.set_xy(13, 177)
 pdf.cell(0, 0, data.get("person_arbeitsausfuehrung"))
 
 pdf.set_font('DGUVMeta-Normal', '', 14)
-pdf.set_xy(14.1, 175.6)
+pdf.set_xy(14.3, 202)
 pdf.cell(0, 0, data.get("zusaetzliche_schutzausrüstung_elektrischerschlag"))
 
 pdf.set_font('DGUVMeta-Normal', '', 14)
-pdf.set_xy(88.4, 175.6)
+pdf.set_xy(72.2, 202)
 pdf.cell(0, 0, data.get("zusaetzliche_schutzausrüstung_stoerlichtbogen"))
 
 pdf.set_font('DGUVMeta-Normal', '', 14)
-pdf.set_xy(14, 200.8)
+pdf.set_xy(14.4, 225.5)
 pdf.cell(0, 0, data.get("abgrenzung_arbeitsbereich_ja"))
 
 pdf.set_font('DGUVMeta-Normal', '', 14)
-pdf.set_xy(37.2, 200.8)
+pdf.set_xy(72.2, 225.5)
 pdf.cell(0, 0, data.get("abgrenzung_arbeitsbereich_nein"))
 
 # 1 Freigeschaltet
