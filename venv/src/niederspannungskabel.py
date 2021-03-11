9430d8f18f4222f5e381bf5ae842a5d5ee58671c
Seppo Walther <seppo.walther@educorvi.de>
2021-03-11 11:46:16 +0100

    Working on Industrie - niederspannungskabel.py: Adding Kopffragen

--- venv/src/niederspannungskabel.py
+++ venv/src/niederspannungskabel.py
@@ -1,9 +1,75 @@
 from fpdf import FPDF
+from importdata import niederspannungskabel as input
 
 pdf = FPDF(orientation='P', unit='mm', format='A4')
 pdf.add_page()
 
-pdf.image("vorlage3-Seite1.jpg", x=-4, y=-8, w=217, h=313)
+pdf.add_font('DGUVMeta-Normal', '', 'DGUVMeta-Normal.ttf', uni=True)
+pdf.add_font('DGUVMeta-Bold', '', 'DGUVMeta-Bold.ttf', uni=True)
+pdf.add_font('DGUVMeta-NormalItalic', '', 'DGUVMeta-NormalItalic.ttf', uni=True)
+
+pdf.image("newtemplate1_seite1.jpg", x=-4, y=-8, w=217, h=313)
+
+data = {}
+input = input.get("data")
+
+# Kopffragen
+
+data["arbeitsstelle"] = input.get('#/properties/arbeitsstelle-arbeitsort')
+data["datum_uhrzeit"] = input.get('#/properties/datum-und-uhrzeit')
+data["person_anlageverantwortlichkeit"] = input.get('#/properties/person-in-der-rolle-des-anlagenverantwortlichen')
+data["person_arbeitsverantwortlichkeit"] = input.get('#/properties/person-in-der-rolle-des-arbeitsverantwortlichen')
+data["person_arbeitsausfuehrung"] = input.get('#/properties/arbeitsausfuhrende-person')
+
+if 'gegen elektrischen Schlag' in input.get('#/properties/zusatzliche-personliche-schutzausrustung-bei-der-1'):
+    data["zusaetzliche_schutzausrüstung_elektrischerschlag"] = "x"
+else:
+    data["zusaetzliche_schutzausrüstung_elektrischerschlag"] = ""
+
+if 'gegen Störlichtbogen' in input.get('#/properties/zusatzliche-personliche-schutzausrustung-bei-der-1'):
+    data["zusaetzliche_schutzausrüstung_stoerlichtbogen"] = "x"
+else:
+    data["zusaetzliche_schutzausrüstung_stoerlichtbogen"] = ""
+
+# Kopffragen
+
+pdf.set_font('DGUVMeta-Normal', '', 14)
+pdf.set_xy(13, 107)
+pdf.set_text_color(0,0,0)
+pdf.cell(0, 0, data.get("arbeitsstelle"))
+
+pdf.set_font('DGUVMeta-Normal', '', 14)
+pdf.set_xy(13, 126)
+pdf.cell(0, 0, data.get("datum_uhrzeit"))
+
+pdf.set_font('DGUVMeta-Normal', '', 14)
+pdf.set_xy(13, 145)
+pdf.cell(0, 0, data.get("person_anlageverantwortlichkeit"))
+
+pdf.set_font('DGUVMeta-Normal', '', 14)
+pdf.set_xy(13, 164)
+pdf.cell(0, 0, data.get("person_arbeitsverantwortlichkeit"))
+
+pdf.set_font('DGUVMeta-Normal', '', 14)
+pdf.set_xy(13, 183)
+pdf.cell(0, 0, data.get("person_arbeitsausfuehrung"))
+
+pdf.set_font('DGUVMeta-Normal', '', 10)
+pdf.set_xy(20, 208.5)
+pdf.cell(0, 0, "gegen elektrischen Schlag")
+
+pdf.set_font('DGUVMeta-Normal', '', 14)
+pdf.set_xy(14.3, 208.3)
+pdf.cell(0, 0, data.get("zusaetzliche_schutzausrüstung_elektrischerschlag"))
+
+pdf.set_font('DGUVMeta-Normal', '', 10)
+pdf.set_xy(78, 208.5)
+pdf.cell(0, 0, "gegen Störlichbogen")
+
+pdf.set_font('DGUVMeta-Normal', '', 14)
+pdf.set_xy(72.2, 208.3)
+pdf.cell(0, 0, data.get("zusaetzliche_schutzausrüstung_stoerlichtbogen"))
+
 
 # 1a Freigeschaltet Ausschaltstelle 1
 
