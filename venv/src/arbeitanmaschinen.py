c51dec64014a0fd4fe5881b18d88ebe4cb758703
Seppo Walther <seppo.walther@educorvi.de>
2021-03-14 23:32:46 +0100

    Adding Kopffragen

--- venv/src/arbeitanmaschinen.py
+++ venv/src/arbeitanmaschinen.py
@@ -13,6 +13,95 @@ pdf.image("newtemplate1_seite1.jpg", x=-4, y=-8, w=217, h=313)
 data = {}
 input = input.get("data")
 
+# Kopffragen
+
+data["arbeitsstelle"] = input.get('#/properties/arbeitsstelle-arbeitsort')
+data["datum_uhrzeit"] = input.get('#/properties/datum-und-uhrzeit')
+data["person_anlageverantwortlichkeit"] = input.get('#/properties/person-in-der-rolle-des-anlagenverantwortlichen')
+data["person_arbeitsverantwortlichkeit"] = input.get('#/properties/persone-in-der-rolle-des-arbeitsverantwortlichen')
+data["person_arbeitsausfuehrung"] = input.get('#/properties/arbeitsausfuhrende-person')
+
+if 'gegen elektrischen Schlag' in input.get('#/properties/zusatzliche-personliche-schutzausrustung-bei-der-1'):
+    data["zusaetzliche_schutzausrüstung_elektrischerschlag"] = "x"
+else:
+    data["zusaetzliche_schutzausrüstung_elektrischerschlag"] = ""
+
+if 'gegen Störlichtbogen' in input.get('#/properties/zusatzliche-personliche-schutzausrustung-bei-der-1'):
+    data["zusaetzliche_schutzausrüstung_stoerlichtbogen"] = "x"
+else:
+    data["zusaetzliche_schutzausrüstung_stoerlichtbogen"] = ""
+
+if input.get('#/properties/stehen-andere-anlagenteile-weiterhin-unter') == "ja":
+    data["abgrenzung_arbeitsbereich_ja"] = "x"
+else:
+    data["abgrenzung_arbeitsbereich_ja"] = ""
+
+if input.get('#/properties/stehen-andere-anlagenteile-weiterhin-unter') == "nein":
+    data["abgrenzung_arbeitsbereich_nein"] = "x"
+else:
+    data["abgrenzung_arbeitsbereich_nein"] = ""
+
+# Kopffragen
+
+pdf.set_font('DGUVMeta-Normal', '', 14)
+pdf.set_xy(13, 107)
+pdf.set_text_color(0,0,0)
+pdf.cell(0, 0, data.get("arbeitsstelle"))
+
+pdf.set_font('DGUVMeta-Normal', '', 14)
+pdf.set_xy(13, 126)
+pdf.cell(0, 0, data.get("datum_uhrzeit"))
+
+pdf.set_font('DGUVMeta-Normal', '', 14)
+pdf.set_xy(13, 145)
+pdf.cell(0, 0, data.get("person_anlageverantwortlichkeit"))
+
+pdf.set_font('DGUVMeta-Normal', '', 14)
+pdf.set_xy(13, 164)
+pdf.cell(0, 0, data.get("person_arbeitsverantwortlichkeit"))
+
+pdf.set_font('DGUVMeta-Normal', '', 14)
+pdf.set_xy(13, 183)
+pdf.cell(0, 0, data.get("person_arbeitsausfuehrung"))
+
+pdf.set_font('DGUVMeta-Normal', '', 10)
+pdf.set_xy(20, 208.5)
+pdf.cell(0, 0, "gegen elektrischen Schlag")
+
+pdf.set_font('DGUVMeta-Normal', '', 14)
+pdf.set_xy(14.3, 208.3)
+pdf.cell(0, 0, data.get("zusaetzliche_schutzausrüstung_elektrischerschlag"))
+
+pdf.set_font('DGUVMeta-Normal', '', 10)
+pdf.set_xy(78, 208.5)
+pdf.cell(0, 0, "gegen Störlichbogen")
+
+pdf.set_font('DGUVMeta-Normal', '', 14)
+pdf.set_xy(72.2, 208.3)
+pdf.cell(0, 0, data.get("zusaetzliche_schutzausrüstung_stoerlichtbogen"))
+
+pdf.set_font('DGUVMeta-Normal', '', 10)
+pdf.set_xy(20, 232)
+pdf.cell(0, 0, "ja")
+
+pdf.set_font('DGUVMeta-Normal', '', 14)
+pdf.set_xy(14.4, 231.6)
+pdf.cell(0, 0, data.get("abgrenzung_arbeitsbereich_ja"))
+
+pdf.set_font('DGUVMeta-Normal', '', 10)
+pdf.set_xy(78, 232)
+pdf.cell(0, 0, "nein")
+
+pdf.set_font('DGUVMeta-Normal', '', 14)
+pdf.set_xy(72.2, 231.6)
+pdf.cell(0, 0, data.get("abgrenzung_arbeitsbereich_nein"))
+
+#Adding new page
+
+pdf.add_page()
+pdf.image("newtemplate1_seite2.jpg", x=-4, y=-8, w=217, h=313)
+
+
 # 1 Freigeschaltet
 
 pdf.set_font('Arial', '', 14)
