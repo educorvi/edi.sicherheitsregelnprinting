b8251a670c9b21c74120f366bbe68e553c9abd35
Seppo Walther <seppo.walther@educorvi.de>
2021-03-15 13:38:31 +0100

    Adding #1

--- venv/src/zaehleranlagen.py
+++ venv/src/zaehleranlagen.py
@@ -110,6 +110,33 @@ pdf.cell(0, 0, data.get("zusaetzliche_schutzausrüstung_stoerlichtbogen"))
 pdf.add_page()
 pdf.image("newtemplate1_seite2.jpg", x=-4, y=-8, w=217, h=313)
 
+# 1 Freigeschaltet
+
+pdf.set_font('DGUVMeta-Bold', '', 10)
+pdf.set_text_color(35,31,32)
+pdf.set_xy(12.7, 29.2)
+pdf.cell(0, 0, 'Wie erfolgte die Freischaltung?')
+
+pdf.set_font('DGUVMeta-Normal', '', 10)
+pdf.set_text_color(0,0,0)
+pdf.set_xy(12.7, 34.2)
+pdf.cell(0, 0, data.get("art_der_freischaltung"))
+
+pdf.set_font('DGUVMeta-Normal', '', 10)
+pdf.set_text_color(0,0,0)
+pdf.set_xy(12.7, 39.2)
+pdf.cell(0, 0, 'Auslösestrom: %s A' % data.get("ausloesestrom"))
+
+pdf.set_font('DGUVMeta-Bold', '', 10)
+pdf.set_text_color(35,31,32)
+pdf.set_xy(12.7, 50)
+pdf.cell(0, 0, 'Wo erfolgte die Freischaltung?')
+
+pdf.set_font('DGUVMeta-Normal', '', 10)
+pdf.set_text_color(0,0,0)
+pdf.set_xy(12.7, 55)
+pdf.cell(0, 0, data.get("ort_der_freischaltung"))
+
 
 
 pdf.output("zaehleranlagen.pdf", "F")
