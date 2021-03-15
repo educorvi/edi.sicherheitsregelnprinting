e88a6520a73a472aee7f92a4b7da6c7a7da2c9e4
Seppo Walther <seppo.walther@educorvi.de>
2021-03-15 09:59:00 +0100

    Exporting zaeleranlagen.pdf

---
+++ venv/src/zaehleranlagen.py
@@ -0,0 +1,18 @@
+from fpdf import FPDF
+from importdata import zaehleranlagen as input
+
+pdf = FPDF(orientation='P', unit='mm', format='A4')
+pdf.add_page()
+
+pdf.add_font('DGUVMeta-Normal', '', 'DGUVMeta-Normal.ttf', uni=True)
+pdf.add_font('DGUVMeta-Bold', '', 'DGUVMeta-Bold.ttf', uni=True)
+pdf.add_font('DGUVMeta-NormalItalic', '', 'DGUVMeta-NormalItalic.ttf', uni=True)
+
+pdf.image("newtemplate1_seite1.jpg", x=-4, y=-8, w=217, h=313)
+
+data = {}
+input = input.get("data")
+
+
+
+pdf.output("zaehleranlagen.pdf", "F")
