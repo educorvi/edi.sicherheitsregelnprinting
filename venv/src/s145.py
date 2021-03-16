7def6c76dedf25dd43bdccb19d88d9d877009d02
Seppo Walther <seppo.walther@educorvi.de>
2021-03-16 10:18:29 +0100

    Working on s145.py

---
+++ venv/src/s145.py
@@ -0,0 +1,234 @@
+from fpdf import FPDF
+
+def create_pdf(input):
+
+    pdf = FPDF(orientation='P', unit='mm', format='A4')
+    pdf.add_page()
+
+    pdf.add_font('DGUVMeta-Normal', '', 'DGUVMeta-Normal.ttf', uni=True)
+    pdf.add_font('DGUVMeta-Bold', '', 'DGUVMeta-Bold.ttf', uni=True)
+    pdf.add_font('DGUVMeta-NormalItalic', '', 'DGUVMeta-NormalItalic.ttf', uni=True)
+
+    pdf.image("newtemplate2_seite1.jpg", x=-4, y=-8, w=217, h=313)
+
+    data = {}
+    input = input.get("data")
+
+    # Kopffragen
+
+    data["arbeitsstelle"] = input.get('#/properties/arbeitsstelle-arbeitsort')
+    data["datum_uhrzeit"] = input.get('#/properties/datum-und-uhrzeit')
+    data["person_anlageverantwortlichkeit"] = input.get('#/properties/person-in-der-rolle-des-anlagenverantwortlichen')
+    data["person_arbeitsverantwortlichkeit"] = input.get('#/properties/person-in-der-rolle-des-arbeitsverantwortlichen')
+    data["person_arbeitsausfuehrung"] = input.get('#/properties/arbeitsausfuhrende-person')
+
+    if 'gegen elektrischen Schlag' in input.get('#/properties/zusatzliche-personliche-schutzausrustung-bei-der-1'):
+        data["zusaetzliche_schutzausrüstung_elektrischerschlag"] = "x"
+    else:
+        data["zusaetzliche_schutzausrüstung_elektrischerschlag"] = ""
+
+    if 'gegen Störlichtbogen' in input.get('#/properties/zusatzliche-personliche-schutzausrustung-bei-der-1'):
+        data["zusaetzliche_schutzausrüstung_stoerlichtbogen"] = "x"
+    else:
+        data["zusaetzliche_schutzausrüstung_stoerlichtbogen"] = ""
+
+    # 1
+
+    data["art_der_freischaltung"] = input.get('#/properties/edi4ed9533af063465dbd40ede7ce144b34')
+
+    if data["art_der_freischaltung"] == "LS-Schalter":
+        data["ausloesestrom"] = input.get('#/properties/edi595058c14f914f97913f1982c3ee63ed')
+    elif data["art_der_freischaltung"] == "NH-Lastschaltleiste":
+        data["ausloesestrom"] = input.get('#/properties/edi866e423a5ffe4c8d8c634335fa41d500')
+    elif data["art_der_freischaltung"] == "Schraubsicherungen":
+        data["ausloesestrom"] = input.get('#/properties/edidd592cc4a5674acfa8669da7e7056728')
+    else:
+        data["ausloesestrom"] = "/"
+
+    data["ort_der_freischaltung"] = input.get('#/properties/edi86c60abee95a45c8886de483c2b84e91')
+
+    # 2
+
+    data["sperrelement"] = input.get('#/properties/edi21ce1be420ec46bab9c030d670cd417f')
+    data["schaltsperre"] = input.get('#/properties/edid27d8666a87a455c96e03b46bc81ed17')
+    data["schalten_verboten"] = input.get('#/properties/edifef867c48d994dd7a9bead41c0a9dc1d')
+
+    # 3
+
+    data["spannungspruefer"] = input.get('#/properties/edi53fa934512eb411eaa2f82e45678d4bd')
+    data["usv"] = input.get('#/properties/edi7c3c497dd89c4eaeb2c142b36a8a44b9')
+
+    # 4
+
+    data["euk_wo_eingebaut"] = input.get('#/properties/edib56dfa856ce84e9a92d61f2920c53f98')
+
+    # 5
+
+    data["ziel_der_abdeckung"] = input.get('#/properties/edi5cac8aded5f245d4964d289ba11c3d9d')
+
+    # Title
+
+    pdf.set_font('DGUVMeta-Bold', '', 20)
+    pdf.set_text_color(0,73,148)
+    pdf.set_xy(12.7, 58.5)
+    pdf.cell(0, 0, 'Arbeiten an')
+
+    pdf.set_font('DGUVMeta-Bold', '', 20)
+    pdf.set_text_color(0,73,148)
+    pdf.set_xy(12.7, 68)
+    pdf.cell(0, 0, 'Endstromkreisen')
+
+    pdf.set_font('DGUVMeta-Bold', '', 14)
+    pdf.set_text_color(0,140,142)
+    pdf.set_xy(12.7, 83.5)
+    pdf.cell(0, 0, 'Elektrohandwerk')
+
+    # Kopffragen
+
+    pdf.set_font('DGUVMeta-Normal', '', 14)
+    pdf.set_xy(13, 107)
+    pdf.set_text_color(0,0,0)
+    pdf.cell(0, 0, data.get("arbeitsstelle"))
+
+    pdf.set_font('DGUVMeta-Normal', '', 14)
+    pdf.set_xy(13, 126)
+    pdf.cell(0, 0, data.get("datum_uhrzeit"))
+
+    pdf.set_font('DGUVMeta-Normal', '', 14)
+    pdf.set_xy(13, 145)
+    pdf.cell(0, 0, data.get("person_anlageverantwortlichkeit"))
+
+    pdf.set_font('DGUVMeta-Normal', '', 14)
+    pdf.set_xy(13, 164)
+    pdf.cell(0, 0, data.get("person_arbeitsverantwortlichkeit"))
+
+    pdf.set_font('DGUVMeta-Normal', '', 14)
+    pdf.set_xy(13, 183)
+    pdf.cell(0, 0, data.get("person_arbeitsausfuehrung"))
+
+    pdf.set_font('DGUVMeta-Normal', '', 10)
+    pdf.set_xy(20, 208.5)
+    pdf.cell(0, 0, "gegen elektrischen Schlag")
+
+    pdf.set_font('DGUVMeta-Normal', '', 14)
+    pdf.set_xy(14.3, 208.3)
+    pdf.cell(0, 0, data.get("zusaetzliche_schutzausrüstung_elektrischerschlag"))
+
+    pdf.set_font('DGUVMeta-Normal', '', 10)
+    pdf.set_xy(78, 208.5)
+    pdf.cell(0, 0, "gegen Störlichbogen")
+
+    pdf.set_font('DGUVMeta-Normal', '', 14)
+    pdf.set_xy(72.2, 208.3)
+    pdf.cell(0, 0, data.get("zusaetzliche_schutzausrüstung_stoerlichtbogen"))
+
+    #Adding new page
+
+    pdf.add_page()
+    pdf.image("newtemplate1_seite2.jpg", x=-4, y=-8, w=217, h=313)
+
+    # 1 Freigeschaltet
+
+    pdf.set_font('DGUVMeta-Bold', '', 10)
+    pdf.set_text_color(35,31,32)
+    pdf.set_xy(12.7, 29.2)
+    pdf.cell(0, 0, 'Wie erfolgte die Freischaltung?')
+
+    pdf.set_font('DGUVMeta-Normal', '', 10)
+    pdf.set_text_color(0,0,0)
+    pdf.set_xy(12.7, 34.2)
+    pdf.cell(0, 0, data.get("art_der_freischaltung"))
+
+    pdf.set_font('DGUVMeta-Bold', '', 10)
+    pdf.set_text_color(35,31,32)
+    pdf.set_xy(12.7, 40.7)
+    pdf.cell(0, 0, 'Wo erfolgte die Freischaltung?')
+
+    pdf.set_font('DGUVMeta-Normal', '', 10)
+    pdf.set_text_color(0,0,0)
+    pdf.set_xy(12.7, 45.7)
+    pdf.cell(0, 0, data.get("ort_der_freischaltung"))
+
+    # 2 Gegen Wiedereinschalten gesichert
+
+    pdf.set_font('DGUVMeta-Bold', '', 10)
+    pdf.set_text_color(35,31,32)
+    pdf.set_xy(12.7, 77.5)
+    pdf.cell(0, 0, 'Wurde ein Sperrelement eingesetzt, weil der Bereich für Laien zugänglich ist?')
+
+    pdf.set_font('DGUVMeta-Normal', '', 10)
+    pdf.set_text_color(0,0,0)
+    pdf.set_xy(12.7, 82.5)
+    pdf.cell(0, 0, data.get("sperrelement"))
+
+    pdf.set_font('DGUVMeta-Bold', '', 10)
+    pdf.set_text_color(35,31,32)
+    pdf.set_xy(12.7, 89)
+    pdf.cell(0, 0, 'Wurde eine Schaltsperre eingesetzt, weil der Bereich für Laien zugänglich ist?')
+
+    pdf.set_font('DGUVMeta-Normal', '', 10)
+    pdf.set_text_color(0,0,0)
+    pdf.set_xy(12.7, 94)
+    pdf.cell(0, 0, data.get("schaltsperre"))
+
+    pdf.set_font('DGUVMeta-Bold', '', 10)
+    pdf.set_text_color(35,31,32)
+    pdf.set_xy(12.7, 100.5)
+    pdf.cell(0, 0, 'Wurde ein Schild "Schalten verboten" zusätzlich angebracht?')
+
+    pdf.set_font('DGUVMeta-Normal', '', 10)
+    pdf.set_text_color(0,0,0)
+    pdf.set_xy(12.7, 105.5)
+    pdf.cell(0, 0, data.get("schalten_verboten"))
+
+    # 3 Spannungsfreiheit allpolig festgestellt an der Arbeitsstelle
+
+    pdf.set_font('DGUVMeta-Bold', '', 10)
+    pdf.set_text_color(35,31,32)
+    pdf.set_xy(12.7, 136)
+    pdf.cell(0, 0, 'Zweipoliger Spannungsprüfer:')
+
+    pdf.set_font('DGUVMeta-Normal', '', 10)
+    pdf.set_text_color(0,0,0)
+    pdf.set_xy(12.7, 141)
+    pdf.cell(0, 0, data.get("spannungspruefer"))
+
+    pdf.set_font('DGUVMeta-Bold', '', 10)
+    pdf.set_text_color(35,31,32)
+    pdf.set_xy(12.7, 147.5)
+    pdf.cell(0, 0, 'Dezentrale Einspeisung vorhanden, z. B. USV, PV, Notstromaggregat?')
+
+    pdf.set_font('DGUVMeta-Normal', '', 10)
+    pdf.set_text_color(0,0,0)
+    pdf.set_xy(12.7, 152.5)
+    pdf.cell(0, 0, data.get("usv"))
+
+    # 4 Geerdet und kurzgeschlossen
+
+    pdf.set_font('DGUVMeta-Bold', '', 10)
+    pdf.set_text_color(35,31,32)
+    pdf.set_xy(12.7, 189)
+    pdf.cell(0, 0, 'Wo wurde die EuK-Vorrichtung eingebaut?')
+
+    pdf.set_font('DGUVMeta-Normal', '', 10)
+    pdf.set_text_color(0,0,0)
+    pdf.set_xy(12.7, 194)
+    pdf.cell(0, 0, data.get("euk_wo_eingebaut"))
+
+    # 5 Mit der Abdeckung soll erreicht werden
+
+    pdf.set_font('DGUVMeta-Bold', '', 10)
+    pdf.set_text_color(35,31,32)
+    pdf.set_xy(12.7, 236.6)
+    pdf.cell(0, 0, 'Mit der Abdeckung soll erreicht werden:')
+
+    pdf.set_font('DGUVMeta-Normal', '', 10)
+    pdf.set_text_color(0,0,0)
+    pdf.set_xy(12.7, 241.6)
+    pdf.cell(0, 0, data.get("ziel_der_abdeckung"))
+
+    pdf.output("s145.pdf", "F")
+
+if __name__ == "__main__":
+    from importdata import endstromkreise as input
+    create_pdf(input)
