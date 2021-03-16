d915181f26c9d3cfc5335e01d7482a9a89701577
Seppo Walther <seppo.walther@educorvi.de>
2021-03-16 01:07:03 +0100

    Preparing to add #3b on next page

--- venv/src/evu_niederspannungskabel.py
+++ venv/src/evu_niederspannungskabel.py
@@ -335,4 +335,16 @@ pdf.set_text_color(0,0,0)
 pdf.set_xy(12.7, 258)
 pdf.cell(0, 0, data.get("spannungspruefer3a"))
 
+# 3b Spannungsfreiheit allpolig festgestellt an der Ausschaltstelle 2
+
+pdf.set_font('DGUVMeta-Bold', '', 10)
+pdf.set_text_color(35,31,32)
+pdf.set_xy(12.7, 278)
+pdf.cell(0, 0, 'Zweipoliger Spannungsprüfer:')
+
+pdf.set_font('DGUVMeta-Normal', '', 10)
+pdf.set_text_color(0,0,0)
+pdf.set_xy(12.7, 271)
+pdf.cell(0, 0, data.get("spannungspruefer3b"))
+
 pdf.output("evu_niederspannungskabel.pdf", "F")
