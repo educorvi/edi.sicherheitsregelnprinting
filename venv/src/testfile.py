a12ded7dd1c2b9c9250df50f11ef4a97d796a2d7
Seppo Walther <seppo.walther@educorvi.de>
2021-04-07 18:59:16 +0200

    Working on testfile.py

--- venv/src/testfile.py
+++ venv/src/testfile.py
@@ -5,4 +5,21 @@ fiverulesjson = fiverules.json()
 elektrohandwerk = fiverulesjson["services"][0]
 s143 = elektrohandwerk["services"][0]
 s143_ui = s143["ui"]
-import pdb; pdb.set_trace()
+
+for i in s143_ui["elements"]:
+    field = i["scope"]
+    fieldid = field
+    field = dict()
+
+    fieldid = fieldid.split("/")
+    fieldid = fieldid[2]
+
+    field["title"] = s143["form"]["properties"][fieldid]["title"]
+    field["type"] = s143["form"]["properties"][fieldid]["type"]
+
+    if fieldid in s143["form"]["required"]:
+        field["required"] = True
+    else:
+        field["required"] = False
+
+    import pdb; pdb.set_trace()
