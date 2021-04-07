9ec82980c798875e2b34a4e3a6cf71f0d449e0d8
Seppo Walther <seppo.walther@educorvi.de>
2021-04-07 18:31:44 +0200

    Working on testfile.py

--- venv/src/testfile.py
+++ venv/src/testfile.py
@@ -1,5 +1,7 @@
 import requests
 
-rudi = requests.get('https://devella.uv-kooperation.de/fiverules')
+rudi = requests.get('https://ella.uv-kooperation.de/fiverules')
 klaus = rudi.json()
+elektrohandwerk = klaus["services"][0]
+s143 = elektrohandwerk["services"][0]
 import pdb; pdb.set_trace()
