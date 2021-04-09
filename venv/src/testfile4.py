54e261cc37b69448e2cc07c9cea0c46c74900fb2
seppowalther <seppo.walther@educorvi.de>
2021-04-09 10:17:33 +0200

    Updating testfile.py; created testfile4.py for implementing reportlab

--- venv/src/testfile4.py
+++ venv/src/testfile4.py
@@ -1,11 +1,21 @@
 import requests
-from fpdf import FPDF
+
+import time
+from reportlab.lib.enums import TA_JUSTIFY
+from reportlab.lib.pagesizes import letter
+from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, Image
+from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
+from reportlab.lib.units import inch
+doc = SimpleDocTemplate("form_letter.pdf",pagesize=letter,
+                        rightMargin=72,leftMargin=72,
+                        topMargin=72,bottomMargin=18)
 
 fiverules = requests.get('https://devella.uv-kooperation.de/fiverules/s143')
 s143 = fiverules.json()
 s143_ui = s143["ui"]
 
 fieldslist = []
+Story = []
 
 variable = 0
 for k in s143_ui["elements"]:
